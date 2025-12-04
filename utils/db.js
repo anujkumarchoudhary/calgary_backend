6ae1241cd5d8107f5d3b729bc6065c3dd45abfb3
anujkumarchoudhary <anujkumarchoudhary6699@gmail.com>
2025-12-04 21:49:15 +0530

    issue fix

--- utils/db.js
+++ utils/db.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const dbConnect = async () => {
- const  MONGO_DB =
-    "mongodb+srv://anujkumarchoudhary6699:tOx14jXbBkM4nvDB@ecommerce.45kqnho.mongodb.net/";
-
   try {
-    await mongoose.connect(MONGO_DB);
+    await mongoose.connect(process.env.MONGO_DB);
     console.log("db connectd");
   } catch (err) {
     console.log("db not connectd");
